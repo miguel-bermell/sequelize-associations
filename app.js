071acc0015616b68f26eb71b5d7af470d1ebac7f
Miguel <miguelbermell@gmail.com>
2021-05-26 13:50:11 +0200

    Sequelize Associations & JSON Web Tokens

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 const loadModels = require("./models/relationship");
 
 var indexRouter = require("./routes/index");
@@ -18,6 +19,7 @@ loadModels();
 
 app.use(logger("dev"));
 app.use(express.json());
+app.use(cors);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
